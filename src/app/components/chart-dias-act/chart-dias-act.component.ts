c9f27aad94f10c982e0aa33e0b949bca27f57c23
darkunchain <luisfer74081@gmail.com>
2021-11-28 00:03:45 -0500

    button 15p

--- src/app/components/chart-dias-act/chart-dias-act.component.ts
+++ src/app/components/chart-dias-act/chart-dias-act.component.ts
@@ -77,7 +77,8 @@ export class ChartDiasActComponent implements OnInit {
       this.Clientes30p = datos.Cli30p
       this.Clientes60 = datos.Cli60
       this.Clientes15T = (datos.Cli15 * 4000) + (datos.Cli15p * 3000)
-      this.Clientes30T = (datos.Cli30 * 7000) + + (datos.Cli30p * 5000)
+      this.Clientes30T = (datos.Cli30 * 7000) + (datos.Cli30p * 5000)
+      console.log(this.Clientes15T,this.Clientes30T)
       this.Clientes60T = datos.Cli60 * 12000
       this.total = this.Clientes15T + this.Clientes30T + this.Clientes60T
       this.dataSource = datos.ClientesHoy
